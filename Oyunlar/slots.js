5c0e637d17a7c12b75ed82626bac2dde3a880720
Monty404 <83779017+Moonty404@users.noreply.github.com>
2021-09-30 05:14:00 +0300

    Update slots.js

--- Oyunlar/slots.js
+++ Oyunlar/slots.js
@@ -25,7 +25,7 @@ if(miktar > para) return message.channel.send(new Discord.MessageEmbed()
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
 .setDescription(`<a:emoji_32:870319275263533107> The amount you entered is not a valid number !?`))
  let kazandin = miktar*3   
-  let slots = ["<:emoji_6:870057018818453525>","<:emoji_5:870056991849054259>","<:emoji_4:870056959229956096>","<:emoji_6:870057018818453525>"];
+  let slots = ["<:srandom:878415028510933062>","<:slots2:878401761788977162>","<:slots3:878401708512927774>","<:AAblank_box:872905535698137138>"];
   //
 //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
   let result1 = Math.floor((Math.random() * slots.length));
@@ -46,17 +46,15 @@ if(miktar > para) return message.channel.send(new Discord.MessageEmbed()
 //===BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\ 
   if (slots[result1] === slots[result2] && slots[result2] === slots[result3] ) {
     var text2 = `**
-    ___SLOTS___  
-|<a:emoji_2:870056896747438121><a:emoji_2:870056896747438121><a:emoji_2:870056896747438121>|${message.author.username}
-|                  |
-|                  |**`;
+ **__SLOTS__**  
+ <a:slots7:878401859818225706><a:slots7:878401859818225706><a:slots7:878401859818225706>|${message.author.username}
+|          | **`;
           message.channel.send(text2).then(msg => {
             setTimeout(() => {
               msg.edit(`**
-    ___SLOTS___  
-|${slots[result1]}${slots[result2]}${slots[result3]}${message.author.username}<a:emoji_10:870059089055592508> ${kazandin}+
-|                  |
-|                  |**`,true);
+ **__SLOTS__**  
+ ${slots[result1]}${slots[result2]}${slots[result3]}${message.author.username} <:rcash2:878753234175553587> ${kazandin}+
+|          | **`,true);
             }, 3500);
           });
 
@@ -64,17 +62,15 @@ if(miktar > para) return message.channel.send(new Discord.MessageEmbed()
 //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
   } else {
     var text3 = `**
-    ___SLOTS___  
-|<a:emoji_2:870056896747438121><a:emoji_2:870056896747438121><a:emoji_2:870056896747438121>|${message.author.username}
-|                  |
-|                  |**`;
+ **__SLOTS__**  
+ <a:slots7:878401859818225706><a:slots7:878401859818225706><a:slots7:878401859818225706>|${message.author.username}
+|          | **`;
           message.channel.send(text3).then(msg => {
             setTimeout(() => {
               msg.edit(`**
-    ___SLOTS___  
-|${slots[result1]}${slots[result2]}${slots[result3]}|${message.author.username}<a:emoji_8:870059060714700870> ${miktar}-
-|                  |
-|                  |**`, true);
+ **__SLOTS__**  
+ ${slots[result1]}${slots[result2]}${slots[result3]}|${message.author.username} <:rcash2:878753234175553587> ${miktar}-
+|          |**`, true);
             }, 3500);
           });
   db.add(`para_${message.author.id}`, -miktar)
