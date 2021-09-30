b569a5f348907a327087829c2119a5dd1c0a86c0
Monty404 <83779017+Moonty404@users.noreply.github.com>
2021-09-30 22:00:45 +0300

    Update beercode.js

--- beercode.js
+++ beercode.js
@@ -575,42 +575,26 @@ client.on("message", async message => {
 });
 ///////////
 var coulpe = [
-  "https://cdn.discordapp.com/attachments/694694675679936585/737306643355664384/image02.gif",
-  "https://cdn.discordapp.com/attachments/694694675679936585/737306628528930816/a_1b0b9e83a11f0e8280c5409e85dc6ecf.gif",
-  "https://cdn.discordapp.com/attachments/694694675679936585/737306606068564108/image0_7.gif",
-  "https://cdn.discordapp.com/attachments/694694675679936585/737306593485651979/image01.gif",
-  "https://cdn.discordapp.com/attachments/694694675679936585/737306585470074890/20.gif",
-  "https://cdn.discordapp.com/attachments/694694675679936585/737305042096488478/image03.gif",
-  "https://cdn.discordapp.com/attachments/694694675679936585/737305094038749224/a_788eee9ad77c81e7406d1c06c75de1a1.gif",
-  "https://cdn.discordapp.com/attachments/694694675679936585/737305150607327255/larissa56.gif",
-  "https://cdn.discordapp.com/attachments/694694675679936585/737305158668517486/image0.gif",
-  "https://cdn.discordapp.com/attachments/694694675679936585/737305302352789554/image0-4.gif",
-  "https://cdn.discordapp.com/attachments/694694675679936585/737306359330111548/gif_dc_dc_dc.gif",
-  "https://cdn.discordapp.com/attachments/694694675679936585/736981583474589777/a_48d8e6c6b3ab9bdc3d27e9dc7bfbee53.gif",
-  "https://cdn.discordapp.com/attachments/694694675679936585/737305354270015628/a_b77c51a4aa5bc3460d375f415dec1507.gif",
-  "https://cdn.discordapp.com/attachments/694694675679936585/737304729444679730/a_73dea0b44c5e1f57bdbbfc036b7cf3dc.gif",
-  "https://cdn.discordapp.com/attachments/694694675679936585/737304802501066862/image0-2.gif",
-  "https://cdn.discordapp.com/attachments/694694675679936585/737109543263404132/image0.gif",
-  "https://cdn.discordapp.com/attachments/694694675679936585/737066393694109718/a_d8f145cc1f4ad05bf97a118f64d673ff.gif",
-  "https://cdn.discordapp.com/attachments/694694675679936585/737066415877652490/image0_8.gif",
-  "https://cdn.discordapp.com/attachments/694694675679936585/737013297336942603/a_37397898637eca6eae7458899b12826c.gif",
-  "https://cdn.discordapp.com/attachments/694694675679936585/736898861913604156/image0-10.gif",
-  "https://cdn.discordapp.com/attachments/694694675679936585/736898644808040538/a_e4931ce8b4c41a2f4bb15c51dfd0b898.gif",
-  "https://cdn.discordapp.com/attachments/694694675679936585/736898505427386378/f6bd4fa420a9d8ce58c6519a90bc8bc0.gif",
-  "https://cdn.discordapp.com/attachments/694694675679936585/736898257925570600/a_1e816625dd242018b144b924043a7933.gif",
-  "https://cdn.discordapp.com/attachments/694694675679936585/736897942342074428/a_8634244b88f7dc8eebbf416640489ef2.gif"
+"https://media.discordapp.net/attachments/738277612039962688/775017819980431360/image0.gif",
+"https://media.discordapp.net/attachments/738277612039962688/775017820161179648/image1.gif",
+"https://media.discordapp.net/attachments/738277612039962688/775017820445868032/image2.gif",
+"https://media.discordapp.net/attachments/738277612039962688/775017820643262465/image3.gif",
+"https://media.discordapp.net/attachments/738277612039962688/775017820853239808/image4.gif",
+"https://media.discordapp.net/attachments/738277612039962688/775017821129932860/image5.gif",       
+"https://media.discordapp.net/attachments/738277612039962688/775017821611753472/image7.gif",
+"https://media.discordapp.net/attachments/725046590242291763/775020613109678090/image0.gif",  
 ];
 client.on("message", async message => {
-  if (message.content === "Rcoulpe") {
+  if (message.content === "Rkiss") {
     if (message.author.bot) return;
     if (!message.channel.guild) return message.reply();
     message.channel
       .send({
         embed: {
-          description: `Gif Coulpe`,
-          color: `#ffff00`,
+          description: `${message.author.username} kiss ${user.username}!`,
+          color: `c6df00`,
           image: {
-            url: coulpe[Math.floor(Math.random() * coulpe.length)]
+            url: kiss💋[Math.floor(Math.random() * Kiss.length)]
           }
         }
       })
