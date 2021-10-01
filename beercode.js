6db832c61a119d85d3ccd01fc1bf399ad691b72d
Monty404 <83779017+Moonty404@users.noreply.github.com>
2021-10-01 03:39:02 +0300

    Update beercode.js

--- beercode.js
+++ beercode.js
@@ -987,21 +987,9 @@ client.on("message", async message => {
   }
 });
 ///////
-var gggg = [
-  "https://cdn.discordapp.com/attachments/737803070217650227/805674220192530462/1b2aa8e3e516bdd7.jpg",
-  "https://cdn.discordapp.com/attachments/737803070217650227/783976924565209118/630670529ef7a3c76a03f8e44ef70ddb.jpg",
-  "https://cdn.discordapp.com/attachments/737803070217650227/755383792209756240/image0.jpg",
-  "https://cdn.discordapp.com/attachments/737803070217650227/783653667952787496/008beb180d5975b98697d2c26dd55bbc.jpg",
-  "https://cdn.discordapp.com/attachments/737803070217650227/790954384397697065/image8.jpg",
-  "https://cdn.discordapp.com/attachments/737803070217650227/814869160811167774/a87704b2e441d9e8e95330332bf555d4.jpg",
-  "https://cdn.discordapp.com/attachments/737803070217650227/737835352533893130/image0.jpg",
-  "https://cdn.discordapp.com/attachments/737803070217650227/790954414621065226/image7.jpg",
-  "https://cdn.discordapp.com/attachments/737803070217650227/783002678770663464/947772bf05bd5c1b58a5e91fcf4ccf59.jpg",
-  "https://cdn.discordapp.com/attachments/737803070217650227/794200284632186890/c11.PNG",
-  ""
-];
+var gggg = ["Never bend your head. Always hold it high. Look the world straight in the eye. - Helen Keller","A year later you wish you had started today","°It will be morning tonight, be patient°","Life is like riding a bicycle. To keep your balance, you must keep moving. - Albert Einstein","It is during our darkest moments that we must focus to see the light.","Believe you can and you're halfway there. - Theodore Roosevelt","Some people look for a beautiful place. Others make a place beautiful. - Hazrat Inayat Khan","I can't change the direction of the wind, but I can adjust my sails to always reach my destination. - Jimmy Dean","Spread love everywhere you go. - Mother Teresa","Gods decide is out of our hindsight power but it is always beneficial for us.","When you have a dream, you've got to grab it and never let go.","When you are not well, memories come to you sooner than anyone else.","If you have good thoughts they will shine out of your face like sunbeams and you will always look lovely. - Roald Dahl","When you catch in a calumny, you know your real friends.","Let us make our future now, and let us make our dreams tomorrow's reality. - Malala Yousafzai","A successful man is one who can lay a firm foundation with the bricks others have thrown at him","In order to succeed, your desire for success should be greater than your fear of failure","If size mattered the elephant would be the king of jungle","You will reap what you plant in your minds farm","No matter what you're going through, there's a light at the end of the tunnel. - Demi Lovato"];
 client.on("message", async message => {
-  if (message.content === "Rhug") {
+  if (message.content === "q") {
     if (message.author.bot) return;
     if (!message.channel.guild) return message.reply();
     message.channel
